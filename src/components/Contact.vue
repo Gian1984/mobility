5755ece6735ff37d7c2ab8d3c74743ff1269b03d
Gianluca <gl.tiengo@gmail.com>
2021-10-21 23:50:13 +0200

    first home page

---
+++ src/components/Contact.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="bg-white px-4 py-5 sm:rounded-lg sm:p-6 max-w-7xl mx-auto">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+      <div class="md:col-span-1">
+        <h3 class="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
+        <p class="mt-1 text-sm text-gray-500">
+          Use a permanent address where you can receive mail.
+        </p>
+      </div>
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <form action="#" method="POST">
+          <div class=" sm:overflow-hidden">
+            <div class="bg-white py-6 px-4 sm:p-6">
+              <div>
+                <h2 id="payment-details-heading" class="text-lg leading-6 font-medium text-gray-900">Payment details</h2>
+                <p class="mt-1 text-sm text-gray-500">Update your billing information. Please note that updating your location could affect your tax rates.</p>
+              </div>
+
+              <div class="mt-6 grid grid-cols-6 gap-6">
+
+                <div class="col-span-4 sm:col-span-3">
+                  <label for="object" class="block text-sm font-medium text-gray-700">Object</label>
+                  <input type="text" name="object" id="object" autocomplete="cc-given-name" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm" />
+                </div>
+
+                <div class="col-span-4 sm:col-span-3">
+                  <label for="full-name" class="block text-sm font-medium text-gray-700">Full name</label>
+                  <input type="text" name="full-name" id="full-name" autocomplete="cc-family-name" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm" />
+                </div>
+
+                <div class="col-span-4 sm:col-span-3">
+                  <label for="email-address" class="block text-sm font-medium text-gray-700">Email address</label>
+                  <input type="text" name="email-address" id="email-address" autocomplete="email" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm" />
+                </div>
+
+                <div class="sm:col-span-3">
+                  <label for="phone-number" class="block text-sm font-medium text-blue-gray-700">
+                    Phone number
+                  </label>
+                  <input type="text" name="phone-number" id="phone-number" autocomplete="tel" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm" />
+                </div>
+
+                <div class="sm:col-span-6">
+                  <label for="description" class="block text-sm font-medium text-blue-gray-900">
+                    Description
+                  </label>
+                  <div class="mt-1">
+                    <textarea id="description" name="description" rows="4" class="block w-full border border-blue-gray-300 rounded-md shadow-sm sm:text-sm focus:ring-blue-500 focus:border-blue-500" />
+                  </div>
+                  <p class="mt-3 text-sm text-blue-gray-500">Brief description for your profile. URLs are hyperlinked.</p>
+                </div>
+
+              </div>
+            </div>
+            <div class="px-4 py-3 text-right sm:px-6">
+              <button type="submit" class="bg-gray-700 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700">
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,321 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main class="mt-16 sm:mt-24">
+    <div class="mx-auto max-w-7xl">
+      <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+        <div class="px-4 sm:px-6 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:items-center">
+          <div>
+            <h1 class="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl">
+              <span class="md:block">Data to enrich your</span>
+              {{ ' ' }}
+              <span class="text-indigo-400 md:block">online business</span>
+            </h1>
+            <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua ad ad non deserunt sunt.
+            </p>
+            <p class="mt-8 text-sm text-white uppercase tracking-wide font-semibold sm:mt-10">Used by</p>
+          </div>
+        </div>
+        <div class="mt-16 sm:mt-24 lg:mt-0 lg:col-span-6">
+          <div class="bg-white sm:max-w-md sm:w-full sm:mx-auto sm:rounded-lg sm:overflow-hidden">
+            <div class="px-4 py-8 sm:px-10">
+              <div>
+                <p class="text-sm font-medium text-gray-700">
+                  Sign in with
+                </p>
+
+                <div class="mt-1 grid grid-cols-3 gap-3">
+                  <div>
+                    <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                      <span class="sr-only">Sign in with Facebook</span>
+                      <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z" clip-rule="evenodd" />
+                      </svg>
+                    </a>
+                  </div>
+
+                  <div>
+                    <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                      <span class="sr-only">Sign in with Twitter</span>
+                      <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                      </svg>
+                    </a>
+                  </div>
+
+                  <div>
+                    <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
+                      <span class="sr-only">Sign in with GitHub</span>
+                      <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd" />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="mt-6 relative">
+                <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div class="w-full border-t border-gray-300" />
+                </div>
+                <div class="relative flex justify-center text-sm">
+                      <span class="px-2 bg-white text-gray-500">
+                        Or
+                      </span>
+                </div>
+              </div>
+
+              <div class="mt-6">
+                <form action="#" method="POST" class="space-y-6">
+                  <div>
+                    <label for="name" class="sr-only">Full name</label>
+                    <input type="text" name="name" id="name" autocomplete="name" placeholder="Full name" required="" class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md" />
+                  </div>
+
+                  <div>
+                    <label for="mobile-or-email" class="sr-only">Mobile number or email</label>
+                    <input type="text" name="mobile-or-email" id="mobile-or-email" autocomplete="email" placeholder="Mobile number or email" required="" class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md" />
+                  </div>
+
+                  <div>
+                    <label for="password" class="sr-only">Password</label>
+                    <input id="password" name="password" type="password" placeholder="Password" autocomplete="current-password" required="" class="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md" />
+                  </div>
+
+                  <div>
+                    <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Create your account
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="px-4 py-6 bg-gray-50 border-t-2 border-gray-200 sm:px-10">
+              <p class="text-xs leading-5 text-gray-500">By signing up, you agree to our <a href="#" class="font-medium text-gray-900 hover:underline">Terms</a>, <a href="#" class="font-medium text-gray-900 hover:underline">Data Policy</a> and <a href="#" class="font-medium text-gray-900 hover:underline">Cookies Policy</a>.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+
+  <!-- Feature section with grid -->
+  <div class="relative bg-white py-16 sm:py-24 lg:py-32">
+    <div class="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+      <p class="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+        Everything you need to deploy your app
+      </p>
+      <p class="mt-5 max-w-prose mx-auto text-xl text-gray-500">
+        Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.
+      </p>
+      <div class="mt-12">
+        <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-2">
+          <div v-for="feature in features" :key="feature.name" class="pt-6">
+            <div class="flow-root bg-gray-50 rounded-lg px-6 pb-8">
+              <div class="-mt-6">
+                <div>
+                        <span class="inline-flex items-center justify-center p-3 bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md shadow-lg">
+                          <component :is="feature.icon" class="h-6 w-6 text-white" aria-hidden="true" />
+                        </span>
+                </div>
+                <h3 class="mt-8 text-lg font-medium text-gray-900 tracking-tight">{{ feature.name }}</h3>
+                <p class="mt-5 text-base text-gray-500">
+                  {{ feature.description }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+
+  <!-- Testimonial section -->
+  <div class="pb-16 bg-gradient-to-r from-teal-500 to-cyan-600 lg:pb-0 lg:z-10 lg:relative">
+    <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-1 lg:gap-8">
+      <div class="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
+        <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
+          <blockquote class="text-center">
+            <div>
+              <svg class="mx-auto h-12 w-12 text-white opacity-25" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+                <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+              </svg>
+              <p class="mt-6 text-2xl font-medium text-white">
+                To give real service you must add something which cannot be bought or measured with money, and that is sincerity and integrity.
+              </p>
+            </div>
+            <footer class="mt-6">
+              <p class="text-base font-medium text-white">Don Alden Adams</p>
+            </footer>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Blog section -->
+  <div class="relative bg-gray-50 py-16 sm:py-24 lg:py-32">
+    <div class="relative">
+      <div class="text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
+        <p class="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
+          Helpful Resources
+        </p>
+        <p class="mt-5 mx-auto max-w-prose text-xl text-gray-500">
+          Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.
+        </p>
+      </div>
+      <div class="mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl">
+        <div v-for="post in blogPosts" :key="post.id" class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+          <div class="flex-shrink-0">
+            <img class="h-48 w-full object-cover" :src="post.imageUrl" alt="" />
+          </div>
+          <div class="flex-1 bg-white p-6 flex flex-col justify-between">
+            <div class="flex-1">
+              <a :href="post.href" class="block mt-2">
+                <p class="text-xl font-semibold text-gray-900">
+                  {{ post.title }}
+                </p>
+                <p class="mt-3 text-base text-gray-500">
+                  {{ post.preview }}
+                </p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!--  Guarantee section-->
+
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto px-4 py-24 sm:px-6 lg:px-8">
+      <div class="bg-gray-50 rounded-2xl px-6 py-16 sm:p-16">
+        <div class="max-w-xl mx-auto lg:max-w-none">
+          <div class="text-center">
+            <h2 class="text-2xl font-extrabold tracking-tight text-gray-900">We built our business on customer service</h2>
+          </div>
+          <div class="mt-12 max-w-sm mx-auto grid grid-cols-1 gap-y-10 gap-x-8 sm:max-w-none lg:grid-cols-4">
+            <div v-for="incentive in incentives" :key="incentive.name" class="text-center sm:flex sm:text-left lg:block lg:text-center">
+              <div class="sm:flex-shrink-0">
+                <div class="flow-root">
+                  <img class="w-16 h-16 mx-auto" :src="incentive.imageSrc" alt="" />
+                </div>
+              </div>
+              <div class="mt-3 sm:mt-0 sm:ml-6 lg:mt-6 lg:ml-0">
+                <h3 class="text-sm font-medium text-gray-900">
+                  {{ incentive.name }}
+                </h3>
+                <p class="mt-2 text-sm text-gray-500">
+                  {{ incentive.description }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <Newsletter></Newsletter>
+  <Contact></Contact>
+
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Contact from "../components/Contact"
+import Newsletter from "../components/Newsletter"
+import {
+  CloudUploadIcon,
+  LockClosedIcon,
+
+} from '@heroicons/vue/outline'
+
+const features = [
+  {
+    name: 'Push to Deploy',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi vitae lobortis.',
+    icon: CloudUploadIcon,
+  },
+  {
+    name: 'SSL Certificates',
+    description: 'Qui aut temporibus nesciunt vitae dicta repellat sit dolores pariatur. Temporibus qui illum aut.',
+    icon: LockClosedIcon,
+  },
+]
+
+const blogPosts = [
+  {
+    id: 1,
+    title: 'Boost your conversion rate',
+    href: '#',
+    imageUrl:
+        'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    preview:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+  },
+  {
+    id: 2,
+    title: 'Boost your conversion rate',
+    href: '#',
+    imageUrl:
+        'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    preview:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+  },
+  {
+    id: 3,
+    title: 'Boost your conversion rate',
+    href: '#',
+    imageUrl:
+        'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+    preview:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+  },
+
+]
+
+const incentives = [
+  {
+    name: 'Free shipping',
+    imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-shipping-simple.svg',
+    description: "It's not actually free we just price it into the products. Someone's paying for it, and it's not us.",
+  },
+  {
+    name: '10-year warranty',
+    imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-warranty-simple.svg',
+    description: "If it breaks in the first 10 years we'll replace it. After that you're on your own though.",
+  },
+  {
+    name: 'Exchanges',
+    imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-exchange-simple.svg',
+    description:
+        "If you don't like it, trade it to one of your friends for something of theirs. Don't send it here though.",
+  },
+  {
+    name: 'Exchanges',
+    imageSrc: 'https://tailwindui.com/img/ecommerce/icons/icon-exchange-simple.svg',
+    description:
+        "If you don't like it, trade it to one of your friends for something of theirs. Don't send it here though.",
+  },
+]
+
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
+    Contact,
+    Newsletter
+  },
+
+  setup() {
+    return {
+      incentives,
+      features,
+      blogPosts
+    }
+  },
 }
+
 </script>
