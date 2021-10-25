d0ed90e652be2eda3d3dc7c282f468ec33103afe
Gianluca <gl.tiengo@gmail.com>
2021-10-25 21:36:52 +0200

    now tab & time pickr works

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,109 +29,110 @@
 
                 <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
 
-                  <div>
-                    <div class="sm:hidden">
-                      <label for="tabs" class="sr-only">Select a tab</label>
-                      <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-                      <select id="tabs" name="tabs" class="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
-                        <option v-for="tab in tabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
-                      </select>
-                    </div>
-                    <div class="hidden sm:block">
-                      <nav class="relative z-0 rounded-lg shadow flex divide-x divide-gray-200" aria-label="Tabs">
-                        <a v-for="(tab, tabIdx) in tabs" :key="tab.name" :href="tab.href" :class="[tab.current ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700', tabIdx === 0 ? 'rounded-l-lg' : '', tabIdx === tabs.length - 1 ? 'rounded-r-lg' : '', 'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10']" :aria-current="tab.current ? 'page' : undefined">
-                          <span>{{ tab.name }}</span>
-                          <span aria-hidden="true" :class="[tab.current ? 'bg-indigo-500' : 'bg-transparent', 'absolute inset-x-0 bottom-0 h-0.5']" />
-                        </a>
-                      </nav>
-                    </div>
-                  </div>
+
+
+
+
+
+
 
                   <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
 
 
 
-                    <form class="space-y-6" action="#" method="POST">
-                      <div>
-                        <label for="email" class="block text-sm font-medium text-gray-700">
-                          Pick up
-                        </label>
-                        <div class="mt-1">
-                          <input id="email" name="email" type="email" autocomplete="email" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                        </div>
-                      </div>
+                    <div class="flex flex-wrap">
+                      <div class="w-full mx-auto">
+                        <ul class="flex mb-0 list-none flex-wrap pl-4 pt-3 pb-4 flex-row">
+                          <li class=" mr-2  text-center">
+                            <button class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(1)" v-bind:class="{'text-pink-600 bg-white': openTab !== 1, 'text-white bg-pink-600': openTab === 1}">
+                              Distance
+                            </button>
+                          </li>
+                          <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                            <button class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(2)" v-bind:class="{'text-pink-600 bg-white': openTab !== 2, 'text-white bg-pink-600': openTab === 2}">
+                              Hourly
+                            </button>
+                          </li>
+                        </ul>
+                        <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6  rounded">
+                          <div class="px-4 py-5 flex-auto">
+                            <div class="tab-content tab-space">
+                              <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+                                <form class="space-y-6" action="#" method="POST">
 
-                      <div>
-                        <label for="password" class="block text-sm font-medium text-gray-700">
-                          Drop off
-                        </label>
-                        <div class="mt-1">
-                          <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
-                        </div>
-                      </div>
+                                  <div>
+                                    <label for="dateofbirth" class="block text-sm font-medium text-gray-700">
+                                      Date
+                                    </label>
+                                    <div class="mt-1">
+                                      <input  type="date" name="dateofbirth" id="dateofbirth" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    </div>
+                                  </div>
 
+                                  <div>
+                                    <label for="email" class="block text-sm font-medium text-gray-700">
+                                      Pick up
+                                    </label>
+                                    <div class="mt-1">
+                                      <input id="email" name="email" type="email" autocomplete="email" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                    </div>
+                                  </div>
 
+                                  <div>
+                                    <label for="password" class="block text-sm font-medium text-gray-700">
+                                      Drop off
+                                    </label>
+                                    <div class="mt-1">
+                                      <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                    </div>
+                                  </div>
 
-                      <div class="flex items-center justify-between">
-                        <div class="flex items-center">
-                          <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
-                          <label for="remember-me" class="ml-2 block text-sm text-gray-900">
-                            Remember me
-                          </label>
-                        </div>
+                                  <div>
+                                    <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                      Book this km
+                                    </button>
+                                  </div>
+                                </form>
+                              </div>
+                              <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+                                <form class="space-y-6" action="#" method="POST">
 
-                        <div class="text-sm">
-                          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                            Forgot your password?
-                          </a>
-                        </div>
-                      </div>
+                                  <div>
+                                    <label for="dateofbirth" class="block text-sm font-medium text-gray-700">
+                                      Date
+                                    </label>
+                                    <div class="mt-1">
+                                      <input  type="date" name="dateofbirth" id="dateofbirth" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    </div>
+                                  </div>
 
-                      <div>
-                        <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                          Sign in
-                        </button>
-                      </div>
-                    </form>
+                                  <div>
+                                    <label for="email" class="block text-sm font-medium text-gray-700">
+                                      Pick up
+                                    </label>
+                                    <div class="mt-1">
+                                      <input id="email" name="email" type="email" autocomplete="email" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                    </div>
+                                  </div>
 
-                    <div class="mt-6">
-                      <div class="relative">
-                        <div class="absolute inset-0 flex items-center">
-                          <div class="w-full border-t border-gray-300" />
-                        </div>
-                        <div class="relative flex justify-center text-sm">
-              <span class="px-2 bg-white text-gray-500">
-                Or continue with
-              </span>
-                        </div>
-                      </div>
+                                  <div>
+                                    <label for="password" class="block text-sm font-medium text-gray-700">
+                                      Drop off
+                                    </label>
+                                    <div class="mt-1">
+                                      <input id="password" name="password" type="password" autocomplete="current-password" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                    </div>
+                                  </div>
 
-                      <div class="mt-6 grid grid-cols-3 gap-3">
-                        <div>
-                          <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-                            <span class="sr-only">Sign in with Facebook</span>
-                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                              <path fill-rule="evenodd" d="M20 10c0-5.523-4.477-10-10-10S0 4.477 0 10c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V10h2.54V7.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V10h2.773l-.443 2.89h-2.33v6.988C16.343 19.128 20 14.991 20 10z" clip-rule="evenodd" />
-                            </svg>
-                          </a>
-                        </div>
-
-                        <div>
-                          <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-                            <span class="sr-only">Sign in with Twitter</span>
-                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                              <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
-                            </svg>
-                          </a>
-                        </div>
-
-                        <div>
-                          <a href="#" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-                            <span class="sr-only">Sign in with GitHub</span>
-                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
-                              <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd" />
-                            </svg>
-                          </a>
+                                  <div>
+                                    <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                      Book this hours
+                                    </button>
+                                  </div>
+                                </form>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -330,8 +331,11 @@
 <script>
 import Contact from "../components/Contact"
 import Newsletter from "../components/Newsletter"
-import { CloudUploadIcon, LockClosedIcon, ChevronRightIcon } from '@heroicons/vue/outline'
+import { CloudUploadIcon, LockClosedIcon, ChevronRightIcon,  } from '@heroicons/vue/outline'
 import { StarIcon } from '@heroicons/vue/solid'
+
+
+
 
 const features = [
   {
@@ -425,21 +429,22 @@ const testimonials = [
     attribution: 'Gilles Marechal',
   },
 
-
 ]
 
-const tabs = [
-  { name: 'Distance', href: '#', current: true },
-  { name: 'Hourly', href: '#', current: false },
-]
+
 
 
 export default {
 
+  name: "pink-tabs",
   data() {
     return {
-      appId: "dZqxD92ayyKfQXwaIWML",
-      appCode:"hKKcAAoKQuxp8teCigHfS781lDYNabjQQu9M1jexl4w",
+      openTab: 1
+    }
+  },
+  methods: {
+    toggleTabs: function(tabNumber){
+      this.openTab = tabNumber
     }
   },
 
@@ -448,71 +453,9 @@ export default {
     Contact,
     Newsletter,
     StarIcon,
-    ChevronRightIcon
+    ChevronRightIcon,
   },
 
-  methods: {
-
-    // drawRoute() {
-    //
-    //   this.axios.get("https://api.mapbox.com/directions/v5/mapbox/driving/-122.42,37.78;-122.45,37.91;-122.48,37.73?access_token=pk.eyJ1IjoiZ2lhbjE5ODQiLCJhIjoiY2t2NGI5MGt1MmJscjJ1cXdsMmQzMWlnZCJ9.wA6vElThtxiQqnWctluVIg")
-    //       .then(response => {
-    //     console.log(response.data);
-    //
-    //   }, error => {
-    //     console.error(error);
-    //   });
-    // },
-
-    drawRoute() {
-
-      // let waypoints = {};
-      // for(let i = 0; i < sequence.length; i++) {
-      //   waypoints["waypoint" + i] = `${sequence[i].lat},${sequence[i].lng}`;
-      // }
-
-      this.axios({
-        method: "GET",
-        headers: {
-          'Content-Type': null
-        },
-        url: "https://matrix.router.hereapi.com/v8/matrix",
-        apiKey:"hKKcAAoKQuxp8teCigHfS781lDYNabjQQu9M1jexl4w",
-        params: {
-          profile: "carFast",
-          origins: [
-            {
-              "lat": 52.5309,
-              "lng": 13.3849
-            },
-            {
-              "lat": 54.0924,
-              "lng": 12.0991
-            }
-          ],
-          "destinations": [
-            {
-              "lat": 51.3397,
-              "lng": 12.3731
-            },
-            {
-              "lat": 51.0504,
-              "lng": 13.7373
-            }
-          ],
-
-
-          // app_id: this.appId,
-          // app_code: this.appCode
-        }
-      }).then(result => {
-        console.log(result.data)
-      }, error => {
-        console.error(error);
-      });
-
-    },
-  },
 
   setup() {
     return {
@@ -520,10 +463,22 @@ export default {
       features,
       blogPosts,
       testimonials,
-      tabs
+
     }
   },
 
 }
 
 </script>
+
+<style>
+[type="date"] {
+  background:#fff url(https://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/calendar_2.png)  97% 50% no-repeat ;
+}
+[type="date"]::-webkit-inner-spin-button {
+  display: none;
+}
+[type="date"]::-webkit-calendar-picker-indicator {
+  opacity: 0;
+}
+</style>
