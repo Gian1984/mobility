3f8fea56d768b4573f8e61ef98bf78c332295ef6
Bryangustin <bryangustin.t@gmail.com>
2021-10-25 23:49:59 +0200

    remove placeholder

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -97,21 +97,20 @@
                                 <form class="space-y-6" action="#" method="POST">
 
                                   <div>
-                                    <!--<label for="email" class="block text-sm font-medium text-gray-700">
+                                    <label for="email" class="block text-sm font-medium text-gray-700">
                                       Pick up
-                                    </label>-->
+                                    </label>
                                     <div class="mt-1">
-                                      <input id="pickup" name="pickup" type="pickup" autocomplete="pickup" placeholder="Adresse de départ" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                      <input id="pickup" name="pickup" type="pickup" autocomplete="pickup" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                     </div>
                                   </div>
 
                                   <div>
-                                    <!--<label for="hours" class="block text-sm font-medium text-gray-700">
+                                    <label for="hours" class="block text-sm font-medium text-gray-700">
                                       Hours
-                                    </label>-->
+                                    </label>
                                     <div class="mt-1">
                                       <select id="hours" name="hours" type="hours" autocomplete="current-dropoff" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                                        <option value="">Combien de temps?</option>
                                         <option value="2">2 heures</option>
                                         <option value="3">3 heures</option>
                                         <option value="4">4 heures</option>
@@ -134,7 +133,7 @@
                                         <option value="21">21 heures</option>
                                         <option value="22">22 heures</option>
                                         <option value="23">23 heures</option>
-                                        <option value="24">24 heures</option>
+                                        <option value="24" selected>24 heures</option>
                                         <option value="36">36 heures</option>
                                         <option value="48">48 heures</option>
                                         <option value="60">60 heures</option>
@@ -146,20 +145,20 @@
                                   </div>
 
                                   <div>
-                                    <!--<label for="date" class="block text-sm font-medium text-gray-1000">
+                                    <label for="date" class="block text-sm font-medium text-gray-1000">
                                       Date
-                                    </label>-->
+                                    </label>
                                     <div class="mt-1">
-                                      <input type="date" name="date" id="date" placeholder="Date de départ" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                      <input type="date" name="date" id="date" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                     </div>
                                   </div>
 
                                   <div>
-                                    <!--<label for="time" class="block text-sm font-medium text-gray-700">
+                                    <label for="time" class="block text-sm font-medium text-gray-700">
                                       Time
-                                    </label>-->
+                                    </label>
                                     <div class="mt-1">
-                                      <input type="time" name="time" id="time" placeholder="Heure de départ" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+                                      <input type="time" name="time" id="time" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                     </div>
                                   </div>
 
