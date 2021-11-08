7c7988fa8545fa0903609fa6429066f7cf0cfa0b
Gianluca <gl.tiengo@gmail.com>
2021-11-08 16:11:35 +0100

    adding data card c

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@
             <a href="/Services" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
               Services
             </a>
-            <a href="/Reservation" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
+            <a href="/Reservation" v-show="this.user != null" class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
               My reservation
             </a>
           </div>
@@ -177,6 +177,12 @@
     }
 
     export default {
+
+      data(){
+        return {
+          user:null,
+        }
+      },
 
       methods:{
         logout(){
