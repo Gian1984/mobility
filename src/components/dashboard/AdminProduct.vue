4430f390b73ad7a8598a4ccbf02c313a0f5602b8
Gianluca <gl.tiengo@gmail.com>
2021-11-12 11:23:52 +0100

    cleaning

--- src/components/dashboard/AdminProduct.vue
+++ src/components/dashboard/AdminProduct.vue
@@ -66,36 +66,6 @@ import { DocumentTextIcon, ClockIcon, MapIcon } from '@heroicons/vue/outline'
 import ProductModal from '../dashboard/ProductModal.vue'
 
 
-const positions = [
-  {
-    id: 1,
-    title: 'Back End Developer',
-    type: 'Full-time',
-    location: 'Remote',
-    department: 'Engineering',
-    closeDate: '2020-01-07',
-    closeDateFull: 'January 7, 2020',
-  },
-  {
-    id: 2,
-    title: 'Front End Developer',
-    type: 'Full-time',
-    location: 'Remote',
-    department: 'Engineering',
-    closeDate: '2020-01-07',
-    closeDateFull: 'January 7, 2020',
-  },
-  {
-    id: 3,
-    title: 'User Interface Designer',
-    type: 'Full-time',
-    location: 'Remote',
-    department: 'Design',
-    closeDate: '2020-01-14',
-    closeDateFull: 'January 14, 2020',
-  },
-]
-
 
 export default {
   data() {
@@ -103,12 +73,6 @@ export default {
       products: [],
       editingItem: null,
       addingProduct: null
-    }
-  },
-
-  setup() {
-    return {
-      positions,
     }
   },
 
