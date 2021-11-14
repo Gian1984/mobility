bb0854035c5ac32ce5575e62c78a9a4f9cb5fda6
Gianluca <gl.tiengo@gmail.com>
2021-11-14 22:47:34 +0100

    working on users

---
+++ src/components/dashboard/UserModal.vue
@@ -0,0 +1,124 @@
+<template>
+
+  <div class="bg-white bg-opacity-75 overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+    <div class="relative w-auto my-6 mx-auto lg:max-w-6xl py-60">
+      <!--content-->
+      <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none bg-gray-200">
+        <!--header-->
+        <div class="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t"></div>
+        <!--body-->
+        <div class="relative p-6 flex-auto">
+
+          <div class="modal-mask">
+            <div class="modal-wrapper">
+              <div class="modal-container">
+                <div class="modal-body">
+                  <h5 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                    <span class="block xl:inline">Modify</span>
+                    {{ ' ' }}
+                    <span class="block text-indigo-600 xl:inline">your users.</span>
+                  </h5>
+                  <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                    Here you can modify all about your users.
+                  </p>
+                  <div class="mt-5">
+                    <div class="flex justify-between">
+                      <label for="firstname" class="block text-sm font-medium text-gray-700">Firstname :</label>
+                    </div>
+                    <div class="mt-1">
+                      <input type="text" v-model="data.firstname" name="firstname" id="firstname" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm" placeholder="Ex: shoes" />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div class="flex justify-between">
+                      <label for="lastname" class="block text-sm font-medium text-gray-700 mt-2">Lastname :</label>
+                    </div>
+                    <div class="mt-1">
+                      <input type="text" v-model="data.lastname" name="pricehour" id="lastname" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-gray-900 focus:border-gray-900 sm:text-sm" placeholder="Ex: 200" />
+                    </div>
+                  </div>
+
+
+
+
+
+
+                </div>
+
+                <div class="modal-footer">
+                  <slot name="footer">
+                    <button type="button" @click="uploadFile" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Done
+                    </button>
+                    <button type="button" @click="close" class="mt-5 ml-3 inline-flex items-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-full shadow-sm text-indigo-600 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                      Leave
+                    </button>
+                  </slot>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+</template>
+
+<script>
+
+import { XIcon } from '@heroicons/vue/outline'
+
+export default {
+  props: ['user'],
+  data() {
+    return {
+      attachment: null
+    }
+  },
+  computed: {
+    data: function() {
+      if (this.user != null) {
+        return this.user
+      }
+      return {
+        firstname:"",
+        lastname:"",
+        email:"",
+        phone:"",
+        streetaddress:"",
+        city:"",
+        region:"",
+        country:"",
+        zip:"",
+        is_admin:"",
+      }
+    }
+  },
+  /*eslint-disable */
+  methods: {
+
+    uploadFile(event) {
+        this.$emit('close', this.user)
+    },
+    close(){
+      this.$emit('close', this.user)
+    }
+  },
+  components:{
+    XIcon
+  }
+}
+</script>
+
+<style>
+
+input[type="file"] {
+  display: none;
+}
+
+</style>
--- src/components/dashboard/Users.vue
+++ src/components/dashboard/Users.vue
@@ -1,3 +1,138 @@
 <template>
-  <div></div>
+
+  <h1 class="mt-10 mb-4 p-2 font-bold">Modifier un outilisateur</h1>
+
+
+
+
+
+
+  <div class="bg-white shadow overflow-hidden sm:rounded-md">
+    <ul role="list" class="divide-y divide-gray-200">
+      <li v-for="(user,index) in users" v-bind:key="index">
+        <div class="px-4 py-4 sm:px-6">
+          <div class="flex items-center justify-between">
+            <p class="text-sm font-medium text-indigo-600 truncate">
+              {{ user.firstname }} {{ user.lastname}}<br>
+              Ref. n° {{ user.id }}
+            </p>
+            <div class="flex-shrink-0 h-10">
+
+            </div>
+          </div>
+          <div class="mt-2 sm:flex sm:justify-between">
+            <div class="sm:flex">
+              <p class="flex items-center text-sm text-gray-500">
+                <MapIcon  class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                Prix / KM €
+              </p>
+              <p class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
+                <ClockIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                Prix / Hour € <input v-model="user.firstname" >
+              </p>
+              <p class="mt-2 flex items-center text-sm text-gray-500 sm:mt-0 sm:ml-6">
+                <DocumentTextIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+                {{ user.lastname }}
+              </p>
+            </div>
+
+            <div class="sm:flex mt-2">
+              <div>
+                <button type="button" @click="editingItem = user"  class="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  Edit
+                  <ExternalLinkIcon class="ml-2 -mr-0.5 h-4 w-4" aria-hidden="true" />
+                </button>
+              </div>
+              <div class="ml-2 mt-2 lg:mt-0">
+                <button type="button" @click="removeUser(user.id, index)" class="inline-flex items-center p-1.5 border border-transparent rounded-full shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                  <TrashIcon class="h-5 w-5" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+  <UserModal @close="endEditing" :user="editingItem" v-show="editingItem != null"></UserModal>
+
+
+
 </template>
+
+<script>
+import { ExternalLinkIcon,TrashIcon } from '@heroicons/vue/outline'
+import { DocumentTextIcon, ClockIcon, MapIcon } from '@heroicons/vue/outline'
+import UserModal from '../dashboard/UserModal.vue'
+
+
+
+export default {
+  data() {
+    return {
+      users: [],
+      editingItem: null,
+    }
+  },
+
+  components: {
+    UserModal,
+    ExternalLinkIcon,
+    DocumentTextIcon,
+    ClockIcon,
+    MapIcon,
+    TrashIcon
+  },
+
+
+  beforeMount() {
+    this.axios.get(process.env.VUE_APP_URL_API + 'api/users/').then(response => this.users = response.data)
+  },
+  methods: {
+
+    endEditing(user) {
+      this.editingItem = null
+
+      let index = this.users.indexOf(user)
+      let firstname = user.firstname
+      let lastname = user.lastname
+      let email = user.email
+      let phone = user.phone
+      let streetaddress = user.streetaddress
+      let city = user.city
+      let region = user.region
+      let country = user.country
+      let zip = user.zip
+      let is_admin = user.is_admin
+      /*eslint-disable */
+      this.axios.put(process.env.VUE_APP_URL_API + `api/users/${user.id}`,
+          {
+            firstname,
+            lastname,
+            email,
+            phone,
+            streetaddress,
+            city,
+            region,
+            country,
+            zip,
+            is_admin,
+          })
+          .then(response => this.users[index] = user)
+    },
+
+    removeUser(userID, index){
+
+
+      this.axios.delete(process.env.VUE_APP_URL_API + "api/users/"+ userID)
+          .then( response => this.users.splice(index, 1))
+
+          .catch(error =>{
+            console.log(error);
+          })
+    },
+  },
+
+}
+</script>
