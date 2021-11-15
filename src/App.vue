0e8e9041e4193dba75a9491d65a21d1a880edba7
Gianluca <gl.tiengo@gmail.com>
2021-11-15 22:11:11 +0100

    working on content & translation

--- src/App.vue
+++ src/App.vue
@@ -112,7 +112,7 @@
     <router-view/>
 
     <footer class="bg-white">
-      <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+      <div class="max-w-7xl mx-auto py-6 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
           <div v-for="item in navigation.main" :key="item.name" class="px-5 py-2">
             <a :href="item.href" class="text-base text-gray-500 hover:text-gray-900">
@@ -127,13 +127,7 @@
           </a>
         </div>
         <p class="mt-8 text-center text-base text-gray-400">
-          &copy; 2021 Belga Mobility, Inc. All rights reserved.
-        </p>
-        <p class="mt-8 text-center text-base text-gray-400">
-          Website carefully crafted 
-          <a href="#" target="_blank">
-            Us
-          </a>
+          &copy; 2021 Belga Mobility, Inc. All rights reserved.  Website carefully crafted  <a class="underline hover:text-gray-900" href="#" target="_blank">Us</a>
         </p>
       </div>
     </footer>
