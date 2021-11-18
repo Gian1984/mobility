838ae4198c0cefaee1513d7b0945ee7067a821f0
Gianluca <gl.tiengo@gmail.com>
2021-11-18 13:03:21 +0100

    test access

--- src/router/index.js
+++ src/router/index.js
@@ -132,16 +132,10 @@ const routes = [
   },
 ]
 
-
-
-
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
-
-
 
     router.beforeEach((to, from, next) => {
       if (to.matched.some(record => record.meta.requiresAuth)) {
@@ -175,130 +169,4 @@ const router = createRouter({
       }
     })
 
-
-
 export default router
-
-
-// const router = createRouter({
-//
-//   history: createWebHistory(),
-//
-//   routes: [
-//
-//     {
-//       path: "/",
-//       name: "Home",
-//       component: Home,
-//     },
-//     {
-//       path: "/About",
-//       name: "About",
-//       component: About,
-//     },
-//     {
-//       path: "/Services",
-//       name: "Services",
-//       component: Services,
-//     },
-//     {
-//       path: "/Dashboard",
-//       name: "Dashboard",
-//       component: Dashboard,
-//       meta: {
-//         requiresAuth: true,
-//         is_admin: true
-//       }
-//     },
-//     {
-//       path: '/Dashboard/:pages',
-//       name: 'Dashboard-pages',
-//       component: Dashboard,
-//       meta: {
-//         requiresAuth: true,
-//         is_admin: true
-//       }
-//     },
-//     {
-//       path: "/Checkout/:id",
-//       name: "Checkout",
-//       component: Checkout,
-//     },
-//     {
-//       path: "/Confirmation",
-//       name: "Confirmation",
-//       component: Confirmation,
-//     },
-//     {
-//       path: "/Login",
-//       name: "Login",
-//       component: Login,
-//     },
-//     {
-//       path: "/Register",
-//       name: "Register",
-//       component: Register,
-//     },
-//     {
-//       path: "/Products/:id",
-//       name: "SingleProducts",
-//       component: SingleProduct,
-//     },
-//
-//     {
-//       path: "/Reservation",
-//       name: "Reservation",
-//       component: Reservation,
-//       meta: {
-//         requiresAuth: true,
-//         is_user: true
-//       }
-//     },
-//     {
-//       path: "/Forgetpassword",
-//       name: "Forgetpassword",
-//       component: Forgetpassword,
-//     },
-//     {
-//       path: "/Reset-password/:token",
-//       name: "ResetPassword",
-//       component: ResetPassword,
-//     },
-//   ]
-// })
-//
-//
-// router.beforeEach((to, from, next) => {
-//   if (to.matched.some(record => record.meta.requiresAuth)) {
-//     if (localStorage.getItem('bigStore.jwt') == null) {
-//       next({
-//         path: '/Login',
-//         params: { nextUrl: to.fullPath }
-//       })
-//     } else {
-//       let user = JSON.parse(localStorage.getItem('bigStore.user'))
-//       if (to.matched.some(record => record.meta.is_admin)) {
-//         if (user.is_admin == 1) {
-//           next()
-//         }
-//         else {
-//           next({ name: '/Reservation' })
-//         }
-//       }
-//       else if (to.matched.some(record => record.meta.is_user)) {
-//         if (user.is_admin == 0) {
-//           next()
-//         }
-//         else {
-//           next({ name: '/Dashboard' })
-//         }
-//       }
-//       next()
-//     }
-//   } else {
-//     next()
-//   }
-// })
-//
-//
-// export default router;
