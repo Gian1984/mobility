50d90fcc3cf24f30fd7c9149ff37bac29991417b
Gianluca <gl.tiengo@gmail.com>
2021-11-18 09:10:43 +0100

    solving double arrow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -703,6 +703,11 @@ export default {
 </script>
 
 <style>
+
+.vc-select select {
+  background-image: none;
+}
+
 [type="date"] {
   background:#fff url(https://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/calendar_2.png)  97% 50% no-repeat ;
 }
@@ -712,5 +717,6 @@ export default {
 [type="date"]::-webkit-calendar-picker-indicator {
   opacity: 0;
 }
+
 </style>
 
